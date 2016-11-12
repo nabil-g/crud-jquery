0ec0a3dfd27d89d2752d8b13b27f9ed2009bf8f3
Nabil Ghedjati <nabil.ghedjati@gmail.com>
2016-11-12 21:27:06 +0100

    update du samedi soir

---
+++ script.js
@@ -0,0 +1,52 @@
+// JS
+
+function republier(cible) {
+  var msg = $(cible).prev().val();
+  $(cible).parent().text(msg);
+}
+
+function modifier(cible) {
+
+  var msg = $(cible).parent().prev();
+  var valmsg = $(cible).parent().prev().val();
+
+  msg.html('<input type="text"><button class="btn btn-primary btn-sm" onclick"republier(this)">Re-publier</button>', function() {
+      $(cible).parent().prev().find('input').val(valmsg);
+  });
+
+
+
+
+}
+
+
+
+function supprimer(cible) {
+  $(cible).parent().parent().remove();
+}
+
+
+var actions = '<td><a href="#" onclick="modifier(this)">Modifier</a><a href="#" onclick="supprimer(this)">Supprimer</a></td></tr>';
+
+$(document).ready(function(){
+
+
+
+  $("#publier").click(function() {
+    event.preventDefault();
+    var nom = $('#inputEmail3').val();
+    var msg = $('#inputPassword3').val();
+
+    if (nom.length > 0 || msg.length > 0) {
+      $('#tableau').append("<tr><td>" + nom + "</td><td>" + msg + "</td>" + actions);
+      $('#inputEmail3').val("");
+      $('#inputPassword3').val("");
+    }
+
+
+  });
+
+
+
+
+});
